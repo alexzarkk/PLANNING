fa182a8e10f5812dd2277b27f23fd4567ee30ede
alexzarkk <alexzarkk@hotmail.com>
2022-10-13 18:48:12 +0800

    a

--- src/comm/bd.js
+++ src/comm/bd.js
@@ -4,6 +4,9 @@ const bd = {
 	api: ['https://ztsapi.5618.co/','https://dev.5618.co/'],
 	appid: '2002281722',
 	appKey: '4kzz5t3t+2002281722+mzaaot', // 浙里办
+	sysname: 'tuo3hudjqex7toqupxw9j4lm',
+	SecretKey: 'BCDSGS_0f05ec12aa9be2b107edb2a07e66ae45',	//servicepwd
+	AccessKey: 'BCDSGA_7d4388d47d989fef0eb063d9e63c0c53',	//servicecode
 	amapKey: 'daffb83c14428939221e09ebc785c89c', //高德
 	
 	sys: {
