fb15328a20a02a10ca04abc1fb6c80cbcb41f8c3
InsHomePgup <7328234@gmail.com>
2022-11-15 10:57:01 +0800

    配置页面标题

--- src/pages/comm/nodata.vue
+++ src/pages/comm/nodata.vue
@@ -1,37 +1,43 @@
 <template>
-<page-meta root-font-size="10px"></page-meta>
-	<view>
-		<cu-custom bgColor="bg-dark" :isBack="true">
-			<block slot="backText"></block>
-			<block slot="content">{{title}}</block>
-		</cu-custom>
-		
-		<view class="cu-bar"></view>
-		<tui-no-data :imgWidth="300" :imgHeight="300" :fixed="true" :imgUrl="bd.imgs.nodata" />
-	</view>
+    <page-meta root-font-size="10px"></page-meta>
+    <view>
+        <cu-custom bgColor="bg-dark" :isBack="true">
+            <block slot="backText"></block>
+            <block slot="content">{{title}}</block>
+        </cu-custom>
+
+        <view class="cu-bar"></view>
+        <tui-no-data :imgWidth="300" :imgHeight="300" :fixed="true" :imgUrl="bd.imgs.nodata" />
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				bd: this.bd,
-				title:''
-				
-			}
-		},
-		onLoad(option) {
-			this.title = option.title||''
-		},
-		methods: {
-			
-		}
-	}
+export default {
+    data() {
+        return {
+            bd: this.bd,
+            title: ''
+
+        }
+    },
+    onLoad(option) {
+        this.title = option.title || ''
+        ZWJSBridge.setTitle({
+            "title": this.title
+        }).then(res => {
+            console.log("修改当前页面的标题成功---", res)
+        }).catch(err => {
+            console.error("修改当前页面的标题失败---", err)
+        })
+    },
+    methods: {
+
+    }
+}
 </script>
 
 <style>
 page {
     background-color: #ffffff;
 }
-
 </style>
